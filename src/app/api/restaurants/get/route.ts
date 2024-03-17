07bd376e121172d56b21aeda06bf50e79645a661
nukliliang <nukliliang@MacBook.local>
2024-03-18 02:01:00 +0800

    test

--- src/app/api/restaurants/get/route.ts
+++ src/app/api/restaurants/get/route.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { restaurantsTable } from "@/db/schema";
 import { sql } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export async function GET() {
   const timestamp = new Date().getTime();
   const respose = await db
