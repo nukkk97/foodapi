87b8b61c2ac626d75286464455b1f555813f6763
nukliliang <nukliliang@MacBook.local>
2024-03-18 01:52:54 +0800

    timestamp

--- src/app/api/restaurants/get/route.ts
+++ src/app/api/restaurants/get/route.ts
@@ -4,13 +4,14 @@ import { sql } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
+  const timestamp = new Date().getTime();
   const respose = await db
     .select({
       name: restaurantsTable.name,
       address: restaurantsTable.address,
     })
     .from(restaurantsTable)
-    .orderBy(sql`RANDOM()`)
+    .orderBy(sql`RANDOM()*${timestamp}`)
     .limit(1)
     .execute();
   return NextResponse.json({
